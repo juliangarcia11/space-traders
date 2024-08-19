aa811dd6994827cf9595c512169a9144d4e204fb
Julian <jlmurraygarcia@gmail.com>
2024-08-18 23:16:57 -0700

    add status and version to home page

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,9 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test("should display the current status of the SpaceTraders API", async ({
+test("should display the basic status elements of the SpaceTraders API", async ({
   page,
 }) => {
   await page.goto("http://localhost:3000");
   const status = await page.locator("data-testid=status").textContent();
   expect(status).toBeTruthy();
+  const version = await page.locator("data-testid=version").textContent();
+  expect(version).toBeTruthy();
+  // const resetDate = await page.locator("data-testid=reset-date").textContent();
+  // expect(resetDate).toBeTruthy();
+  const title = await page.locator("data-testid=home-title").textContent();
+  expect(title).toBeTruthy();
+  const description = await page
+    .locator("data-testid=home-description")
+    .textContent();
+  expect(description).toBeTruthy();
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,61 @@
 import { PageLink } from "~/components/page-link";
 import { getStatus } from "~/api/get-status";
 import { Message } from "primereact/message";
+import { Tag } from "primereact/tag";
+import Link from "next/link";
 
 export default async function HomePage() {
   const response = await getStatus();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center">
       <Message
-        text={response.success ? response.data.status : response.error}
+        text={
+          <span className="flex flex-row">
+            <span className="my-auto">
+              {response.success ? response.data.status : response.error}
+            </span>
+            {response.success && (
+              <Tag
+                value={response.data.version}
+                severity="success"
+                className="ml-2"
+                data-testid="version"
+              />
+            )}
+          </span>
+        }
         severity={response.success ? "success" : "error"}
         data-testid="status"
       />
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 pt-4">
         <h1
           className="text-5xl font-extrabold tracking-tight sm:text-[5rem]"
           data-testid="home-title"
         >
-          Hello <span className="text-[hsl(280,100%,70%)]">World</span> App
+          Welcome,{" "}
+          <span className="text-[hsl(220,100%,70%)]">Space Traders!</span>
         </h1>
 
         <p
-          className="max-w-2xl text-center text-2xl"
+          className="max-w-2xl text-center text-2xl font-thin"
           data-testid="home-description"
         >
-          This is a Next.js starter template with Tailwind CSS, TypeScript,
-          ESLint, Jest, Playwrite, and more.
+          This is UI companion application for the{" "}
+          <Link
+            href="https://spacetraders.io"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-normal italic"
+          >
+            SpaceTraders API
+          </Link>
         </p>
 
         <div className="flex flex-row justify-center gap-2">
           <PageLink
             href="/about"
             title="About"
-            subtitle="Learn more about this template"
+            subtitle="Learn more about this game and the SpaceTraders API"
             data-testid="about-link"
           />
           <PageLink
