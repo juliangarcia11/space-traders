1675076282a8089223a43a951be637326083b8a9
Julian <jlmurraygarcia@gmail.com>
2024-08-21 03:59:08 -0700

    update about page spec

--- e2e/about.spec.ts
+++ e2e/about.spec.ts
@@ -1,12 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("should navigate to the home page", async ({ page }) => {
-  // Start from the about page (the baseURL is set via the webServer in the playwright.config.ts)
+test("should navigate to the about page", async ({ page }) => {
   await page.goto("/about");
-  // Find an element with the text 'Home' and click on it
-  await page.getByTestId("home-link").click();
-  // The new URL should be "/" (baseURL is used there)
-  await expect(page).toHaveURL("http://127.0.0.1:3000/");
-  // The new page should contain a h1 with testid "home-title"
-  await expect(page.locator('[data-testid="home-title"]')).toBeVisible();
+  await expect(page.locator('[data-testid="about-title"]')).toBeVisible();
+  await expect(page.locator('[data-testid="api-description"]')).not.toBeEmpty();
+  await expect(page.locator('[data-testid="app-description"]')).not.toBeEmpty();
 });
