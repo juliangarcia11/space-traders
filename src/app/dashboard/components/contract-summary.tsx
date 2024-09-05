ef1e6fe6655b11e849fe49b37fb2068edd6adfba
Julian <jlmurraygarcia@gmail.com>
2024-09-05 16:49:37 -0700

    restyle contract summary to look like ship summary

--- src/app/dashboard/components/contract-summary.tsx
+++ src/app/dashboard/components/contract-summary.tsx
@@ -12,7 +12,7 @@ export function ContractSummary({ contract }: { contract: TContract }) {
     acceptedMsg: "border-blue-500",
     fulfilledMsg: "border-green-500",
     expiredMsg: "border-red-500",
-    defaultMsg: "border-gray-300",
+    defaultMsg: "border-black/10 dark:border-white/10",
   });
   const background = calculateAcceptance({
     isAccepted: contract.accepted,
@@ -21,11 +21,12 @@ export function ContractSummary({ contract }: { contract: TContract }) {
     acceptedMsg: "bg-blue-50 dark:bg-blue-900",
     fulfilledMsg: "bg-green-50 dark:bg-green-900",
     expiredMsg: "bg-red-50 dark:bg-red-900",
-    defaultMsg: "",
+    defaultMsg:
+      "bg-gray-50 hover:bg-gray-50/50 dark:bg-white/5 dark:hover:bg-white/10",
   });
   return (
     <div
-      className={`${border} ${background} flex flex-row gap-2 rounded-md border border-gray-300 p-4 md:gap-4`}
+      className={`${border} ${background} my-2 flex flex-row gap-2 rounded-xl border p-4 shadow-md transition duration-300 ease-in-out hover:shadow-lg md:gap-4`}
     >
       <ContractTypeIcon type={contract.type} />
       <ContractTitle {...contract} />
